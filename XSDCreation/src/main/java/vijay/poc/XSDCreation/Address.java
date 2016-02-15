e042fdd5ce8542eb5bc9b95c9495b686e880ace8
Vijay <AM.Vijay@gmail.com>
2016-02-15 01:31:13 -0500

    XSDCreation
    XSDCreation using Eclipse JAXB

---
+++ XSDCreation/src/main/java/vijay/poc/XSDCreation/Address.java
@@ -0,0 +1,34 @@
+package vijay.poc.XSDCreation;
+
+public class Address {
+	
+	private String streetName;
+	private String cityName;
+	private String zipCode;
+	private String state;
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
